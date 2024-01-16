2d750fb230489027e00e7d77cd0d9150aa2eb29e
Jose Antonio <josanrocu@gmail.com>
2024-01-16 10:04:10 +0100

    correcciones hechas

--- src/main/java/cat/itacademy/barcelonactiva/rodriguez/jose/s04/t01/n02/Controllers/HelloWorldController.java
+++ src/main/java/cat/itacademy/barcelonactiva/rodriguez/jose/s04/t01/n02/Controllers/HelloWorldController.java
@@ -3,8 +3,9 @@ package cat.itacademy.barcelonactiva.rodriguez.jose.s04.t01.n02.Controllers;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1")
 public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
@@ -13,14 +14,13 @@ public class HelloWorldController {
         return "Hola, " + nom.orElseGet(() -> "UNKNOWN") + ". Estàs executant un projecte Gradle";
     }
 
-    @GetMapping("/HelloWorld2")
     @RequestMapping(value = {"/HelloWorld2", "/HelloWorld2/{nom}"})
     public String saludo2(@PathVariable(required = false) String nom) {
         if (nom != null) {
             return "Hola, " + nom + ". Estàs executant un projecte Gradle";
-        } else {
-            return "Hola, estàs executant un projecte Gradle";
         }
+        return "Hola, estàs executant un projecte Gradle";
+
     }
 
 }
